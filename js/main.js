83716aad68f43461ef28cc08f8d7183376e92dd9
Sergey Filimonov <srfilimonoff@gmail.com>
2023-04-30 12:29:06 +0300

    home task 1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,50 @@
 "use strict";
 
 window.addEventListener("load", function () {
-  let inp1 = this.document.querySelector(".num1");
-  let inp2 = this.document.querySelector(".num2");
-  let btnRun = this.document.querySelector(".btnRun");
-  let resultBox = this.document.querySelector(".result");
+  const num1 = document.querySelector(".num1");
+  const num2 = document.querySelector(".num2");
+  const signs = document.querySelector(".signs");
+  const btnRun = document.querySelector(".btnRun");
+  const result = document.querySelector(".result");
 
-  btnRun.addEventListener("click", function () {
-    let total = parseInt(inp1.value) + parseInt(inp2.value);
-    resultBox.innerHTML = total;
+  function updateButtonState() {
+    if (num1.value && num2.value) {
+      btnRun.disabled = false;
+    } else {
+      btnRun.disabled = true;
+    }
+  }
+
+  updateButtonState();
+
+  function handleInput(event) {
+    event.target.value = event.target.value.replace(/\D/g, "");
+    updateButtonState();
+  }
+
+  num1.addEventListener("input", handleInput);
+  num2.addEventListener("input", handleInput);
+
+  btnRun.addEventListener("click", () => {
+    const n1 = Number(num1.value);
+    const n2 = Number(num2.value);
+    const op = signs.value;
+    let res;
+    switch (op) {
+      case "+":
+        res = n1 + n2;
+        break;
+      case "-":
+        res = n1 - n2;
+        break;
+      case "*":
+        res = n1 * n2;
+        break;
+      case "/":
+        res = n1 / n2;
+        break;
+    }
+    result.textContent = `Результат: ${res}`;
+    btnRun.disabled = true;
   });
 });
